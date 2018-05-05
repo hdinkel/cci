58f809e59d15dd5c8e3573d709b867c8bc67b4bc
Holger Dinkel <6j5fnb2xrg@snkmail.com>
2018-05-05 17:29:51 +0200

    add serializers

---
+++ alerts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Alert
+
+class AlertSerializer(serializers.HyperlinkedModelSerializer):
+    user_name = serializers.CharField(max_length=50)  # Should be ForeignKey to User
+    user_email = serializers.EmailField()
+    update_time = serializers.CharField(max_length=2)
+    phrase = serializers.CharField(max_length=200)
+
+    class Meta:
+        model = Alert
+        fields = ('user_name', 'user_email', 'update_time', 'phrase')
+
