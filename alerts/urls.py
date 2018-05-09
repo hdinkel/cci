40928e6e2d702e387a29f7b07c8db26c99bcadd7
Holger Dinkel <6j5fnb2xrg@snkmail.com>
2018-05-09 17:08:36 +0200

    add delete button

--- alerts/urls.py
+++ alerts/urls.py
@@ -8,5 +8,5 @@ from . import views
 urlpatterns = (
     path('', views.AlertList.as_view(), name='alert_list'),
     path('new', views.AlertCreate.as_view(), name='alert_create'),
-    path('{int:pk}', views.AlertUpdate.as_view(), name='alert_update'),
-    path('{pk}/delete', views.AlertDelete.as_view(), name='alert_delete'),)
+    path('<int:pk>', views.AlertUpdate.as_view(), name='alert_update'),
+    path('<int:pk>/delete', views.AlertDelete.as_view(), name='alert_delete'),)
